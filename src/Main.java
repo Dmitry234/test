4b94010f28c7b271b98a2d370c4ba3f48d57e175
Dmitry234 <g-d-a@bk.ru>
2021-12-14 10:53:34 +0700

    branch2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         tank.printPosition();
         tank.goForward(50);
         tank.printPosition();
-        tank.goBackward(80);
+        tank.goBackward(110);
         tank.printPosition();
 
     }
